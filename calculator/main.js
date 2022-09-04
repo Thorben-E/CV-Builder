edfb51a68a2dfa1130740cbb8e35811b29adbf88
Thorben <thorbenegberink@gmail.com>
2022-09-04 21:13:04 +0100

    basics

---
+++ calculator/main.js
@@ -0,0 +1,30 @@
+// functions for mathematical equations
+function add(a, b) {
+    return a + b
+}
+
+function minus(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function devide(a, b) {
+    return a / b
+}
+
+// operate function
+function operate(operator, a, b) {
+    return operator(a, b)
+}
+
+// display functions
+let displayValue = 0
+
+function numberPress(number) {
+    
+    
+    displayValue = number
+}
